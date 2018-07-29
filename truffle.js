521e0870e9fff26208ae5a7357303635a61991fd
pbrudny <pbrudny@gmail.com>
2018-07-30 00:04:45 +0100

    Add infura support

--- truffle.js
+++ truffle.js
@@ -1,14 +1,15 @@
 require('babel-register');
 require('babel-polyfill');
 require('dotenv').config();
-const HDWalletProvider = require("truffle-hdwallet-provider");
+const hdkey = require('ethereumjs-wallet/dist/hdkey');
 
+const HDWalletProvider = require("truffle-hdwallet-provider");
 
 module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
-      port: 8545,
+      port: 7545,
       network_id: "*" // Match any network id
     },
 
